49cb7888e39e15eee878cf7aaf6d061be70b55d8
vladem <lairotele@gmail.com>
2020-02-04 23:01:32 +0300

    use priority queue instead of filtering list each time

--- 126. Word Ladder II/index.js
+++ 126. Word Ladder II/index.js
@@ -16,6 +16,80 @@ Object.defineProperty(Array.prototype, 'flat', {
   },
 });
 
+// Priority Queue taken from https://stackoverflow.com/questions/42919469/efficient-way-to-implement-priority-queue-in-javascript
+const top = 0;
+const parent = (i) => ((i + 1) >>> 1) - 1;
+const left = (i) => (i << 1) + 1;
+const right = (i) => (i + 1) << 1;
+
+class PriorityQueue {
+  constructor(comparator = (a, b) => a > b) {
+    this._heap = [];
+    this._comparator = comparator;
+  }
+  size() {
+    return this._heap.length;
+  }
+  isEmpty() {
+    return this.size() == 0;
+  }
+  peek() {
+    return this._heap[top];
+  }
+  push(...values) {
+    values.forEach((value) => {
+      this._heap.push(value);
+      this._siftUp();
+    });
+    return this.size();
+  }
+  pop() {
+    const poppedValue = this.peek();
+    const bottom = this.size() - 1;
+    if (bottom > top) {
+      this._swap(top, bottom);
+    }
+    this._heap.pop();
+    this._siftDown();
+    return poppedValue;
+  }
+  replace(value) {
+    const replacedValue = this.peek();
+    this._heap[top] = value;
+    this._siftDown();
+    return replacedValue;
+  }
+  _greater(i, j) {
+    return this._comparator(this._heap[i], this._heap[j]);
+  }
+  _swap(i, j) {
+    [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
+  }
+  _siftUp() {
+    let node = this.size() - 1;
+    while (node > top && this._greater(node, parent(node))) {
+      this._swap(node, parent(node));
+      node = parent(node);
+    }
+  }
+  _siftDown() {
+    let node = top;
+    while (
+      (left(node) < this.size() && this._greater(left(node), node)) ||
+      (right(node) < this.size() && this._greater(right(node), node))
+    ) {
+      let maxChild =
+        right(node) < this.size() && this._greater(right(node), left(node))
+          ? right(node)
+          : left(node);
+      this._swap(node, maxChild);
+      node = maxChild;
+    }
+  }
+}
+
+////////
+
 const isNeighbours = (wordA, wordB, maxDistance = 1) => {
   let distance = 0;
 
@@ -73,16 +147,8 @@ const createGraph = (wordList) => {
  * @returns {Object<string, number>}
  */
 const findDistances = (adjacencyList, beginWord) => {
-  const words = Object.keys(adjacencyList);
+  const unvisited = new PriorityQueue((a, b) => distances[a] < distances[b]);
   const distances = {};
-  const visited = {};
-
-  const closestUnvisited = () => {
-    return words
-      .filter((w) => !visited[w])
-      .sort((a, b) => (distances[a] > distances[b] ? 1 : -1))
-      .shift();
-  };
 
   for (const word in adjacencyList) {
     if (word !== beginWord) {
@@ -90,24 +156,23 @@ const findDistances = (adjacencyList, beginWord) => {
     } else {
       distances[word] = 0;
     }
-    visited[word] = false;
+    unvisited.push(word);
   }
 
   while (true) {
-    const current = closestUnvisited();
+    const current = unvisited.pop();
 
     if (!current) {
       break;
     }
 
-    visited[current] = true;
     const currentDistance = distances[current];
 
     for (const neighbour of adjacencyList[current]) {
       const neighbourDistance = currentDistance + 1;
 
       if (distances[neighbour] > neighbourDistance) {
-        visited[neighbour] = false;
+        unvisited.push(neighbour);
         distances[neighbour] = neighbourDistance;
       }
     }
@@ -161,8 +226,8 @@ const findLadders = function(beginWord, endWord, wordList) {
   return paths;
 };
 
-const words = ['a', 'b', 'c'];
+const words = require('./words');
 
-const res = findLadders('a', 'b', words);
+const res = findLadders('sand', 'acne', words);
 
 const test = 1;
