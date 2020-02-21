8c795001e180c3fc37e214d163d12252a2c64b3e
vladem <lairotele@gmail.com>
2020-02-21 21:48:40 +0300

    add tests for task 662

---
+++ 662. Maximum Width of Binary Tree/index.test.js
@@ -0,0 +1,24 @@
+const { widthOfBinaryTree, TreeNode } = require('./index');
+
+describe('662. Maximum Width of Binary Tree', () => {
+  test('binary tree width should be correctly counted', () => {
+    let treeArray;
+    let tree;
+
+    treeArray = [1, 3, 2, 5, 3, null, 9];
+    tree = TreeNode.treeFromArray(treeArray);
+    expect(widthOfBinaryTree(tree)).toBe(4);
+
+    treeArray = [1, 3, null, 5, 3];
+    tree = TreeNode.treeFromArray(treeArray);
+    expect(widthOfBinaryTree(tree)).toBe(2);
+
+    treeArray = [1, 3, 2, 5, null, null, 9, 6, null, null, 7];
+    tree = TreeNode.treeFromArray(treeArray);
+    expect(widthOfBinaryTree(tree)).toBe(8);
+
+    let { treeArray1 } = require('./leetcode_bullshit_input');
+    tree = TreeNode.treeFromArray(treeArray1);
+    expect(widthOfBinaryTree(tree)).toBe(1);
+  });
+});
